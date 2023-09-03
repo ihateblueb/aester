c20a046a250c34d5f904ceefbdda3fd73ad8f3df
blueb <118416443+ihateblueb@users.noreply.github.com>
2023-09-02 21:37:03 -0400

    add more STUFF

--- pages/index.vue
+++ pages/index.vue
@@ -1,145 +1,148 @@
 <script>
 export default {
-  data: () => ({
-    code: "", // gets ignored after login
-    loginstate: 'start',
-    instanceurl: "",
-    token: "",
-    user: {
-      displayname: "none",
-      username: "none",
-    },
-    app: {
-      clientid: "",
-      secret: "",
-    },
-    tootarea: ""
-  }),
-  mounted() {
-    console.log("[Aester Startup] Login State: "+this.getLocalStorage("loginstate"))
-
-    if (this.getLocalStorage("loginstate") === 'done') {
-      this.loginstate = this.getLocalStorage("loginstate");
-      this.afterLogin()
-      this.getAccountDetails()
-    }
-    
-  },
-  methods: {
-    setLocalStorage(key, value) {
-      if(process.client) {
-        localStorage.setItem(key, value)
-      }
-    },
-    getLocalStorage(key) {
-      if(process.client) {
-        return localStorage.getItem(key);
-      }
-    },
-    removeLocalStorage(key) {
-      if(process.client) {
-        return localStorage.removeItem(key);
-      }
-    },
-
-    async createApplication() {
-      const gettingapp = await fetch("https://" + this.instanceurl + "/api/v1/apps", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/x-www-form-urlencoded",
+    data: () => ({
+        code: "", // gets ignored after login
+        loginstate: 'start',
+        instanceurl: "",
+        token: "",
+        user: {
+            displayname: "none",
+            username: "none",
+            avatar: "none",
         },
-        body: `client_name=Aster for Web&redirect_uris=urn:ietf:wg:oauth:2.0:oob&scopes=read write push&website=https://as.blueb.me/`,
-      })
-      const gettingapp_response = await gettingapp.json()
-
-      this.app.clientid = gettingapp_response.client_id;
-      this.app.secret = gettingapp_response.client_secret;
-      this.app.vapidkey = gettingapp_response.vapid_key;
-      this.setLocalStorage("app_clientid", this.app.clientid)
-      this.setLocalStorage("app_secret", this.app.secret)
-      this.setLocalStorage("app_vapidkey", this.app.vapidkey)
-    },
-
-    async startlogin() {
-      await this.createApplication()
-
-      window.open("https://" + this.instanceurl + "/oauth/authorize?client_id="+this.app.clientid+"&scope=read+write+push&redirect_uri=urn:ietf:wg:oauth:2.0:oob&response_type=code", '_blank');
-      this.loginstate = 'almost';
-      this.setLocalStorage("instanceurl", this.instanceurl)
-    },
-
-    async endlogin() {
-
-      const gettingtoken = await fetch("https://" + this.instanceurl + "/oauth/token", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/x-www-form-urlencoded",
+        app: {
+            clientid: "",
+            secret: "",
         },
-        body: `client_id=${this.app.clientid}&client_secret=${this.app.secret}&redirect_uri=urn:ietf:wg:oauth:2.0:oob&grant_type=authorization_code&code=${this.code}&scope=read write push`,
-      })
-      const gettingtoken_response = await gettingtoken.json()
+        tootarea: ""
+    }),
+    mounted() {
+        console.log("[Aester Startup] Login State: " + this.getLocalStorage("loginstate"))
 
-      this.setLocalStorage("token", gettingtoken_response.access_token)
-      this.setLocalStorage("loginstate", "done")
-      
-      this.token = gettingtoken_response.access_token;
-      this.loginstate = 'done';
-
-      this.getAccountDetails()
-      this.afterLogin()
-    },
-
-    async getAccountDetails() {
-      const accountdetails = await fetch("https://" + this.instanceurl + "/api/v1/accounts/verify_credentials", {
-        method: "GET",
-        headers: {
-          "Authorization": "Bearer "+this.token,
+        if (this.getLocalStorage("loginstate") === 'done') {
+            this.loginstate = this.getLocalStorage("loginstate");
+            this.afterLogin()
+            this.getAccountDetails()
         }
-      })
-      const accountdetails_response = await accountdetails.json()
 
-      this.user.displayname = accountdetails_response.display_name;
-      this.user.username = accountdetails_response.username;
     },
-
-    async afterLogin() {
-      this.instanceurl = this.getLocalStorage("instanceurl");
-      this.token = this.getLocalStorage("token");
-      
-      this.app.clientid = this.getLocalStorage("app_clientid");
-      this.app.secret = this.getLocalStorage("app_secret");
-      this.app.vapidkey = this.getLocalStorage("app_vapidkey");
-    },
-
-    async logout() {
-      console.log("Logging out...")
-      this.removeLocalStorage("token")
-      this.removeLocalStorage("instanceurl")
-      this.removeLocalStorage("loginstate")
-      
-      this.removeLocalStorage("app_clientid")
-      this.removeLocalStorage("app_secret")
-      this.removeLocalStorage("app_vapidkey")
-
-      location.reload(); 
-    },
-
-    async sendToot() {
-      console.log("[Aester Actions] Posting toot as @"+this.user.username+"@"+this.instanceurl+"...")
-
-      const sendtoot = await fetch("https://" + this.instanceurl + "/api/v1/statuses", {
-        method: "POST",
-        headers: {
-          "Authorization": "Bearer "+this.token,
-          "Content-Type": "application/x-www-form-urlencoded",
+    methods: {
+        setLocalStorage(key, value) {
+            if (process.client) {
+                localStorage.setItem(key, value)
+            }
         },
-        body: `status=${this.tootarea}`,
-      })
-      const sendtoot_response = await sendtoot.json()
+        getLocalStorage(key) {
+            if (process.client) {
+                return localStorage.getItem(key);
+            }
+        },
+        removeLocalStorage(key) {
+            if (process.client) {
+                return localStorage.removeItem(key);
+            }
+        },
 
-      console.log("[Aester Actions] Posted toot. Response: "+sendtoot_response)
+        async createApplication() {
+            const gettingapp = await fetch("https://" + this.instanceurl + "/api/v1/apps", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/x-www-form-urlencoded",
+                },
+                body: `client_name=Aster for Web&redirect_uris=urn:ietf:wg:oauth:2.0:oob&scopes=read write push&website=https://as.blueb.me/`,
+            })
+            const gettingapp_response = await gettingapp.json()
+
+            this.app.clientid = gettingapp_response.client_id;
+            this.app.secret = gettingapp_response.client_secret;
+            this.app.vapidkey = gettingapp_response.vapid_key;
+            this.setLocalStorage("app_clientid", this.app.clientid)
+            this.setLocalStorage("app_secret", this.app.secret)
+            this.setLocalStorage("app_vapidkey", this.app.vapidkey)
+        },
+
+        async startlogin() {
+            await this.createApplication()
+
+            window.open("https://" + this.instanceurl + "/oauth/authorize?client_id=" + this.app.clientid + "&scope=read+write+push&redirect_uri=urn:ietf:wg:oauth:2.0:oob&response_type=code", '_blank');
+            this.loginstate = 'almost';
+            this.setLocalStorage("instanceurl", this.instanceurl)
+        },
+
+        async endlogin() {
+
+            const gettingtoken = await fetch("https://" + this.instanceurl + "/oauth/token", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/x-www-form-urlencoded",
+                },
+                body: `client_id=${this.app.clientid}&client_secret=${this.app.secret}&redirect_uri=urn:ietf:wg:oauth:2.0:oob&grant_type=authorization_code&code=${this.code}&scope=read write push`,
+            })
+            const gettingtoken_response = await gettingtoken.json()
+
+            this.setLocalStorage("token", gettingtoken_response.access_token)
+            this.setLocalStorage("loginstate", "done")
+
+            this.token = gettingtoken_response.access_token;
+            this.loginstate = 'done';
+
+            this.getAccountDetails()
+            this.afterLogin()
+        },
+
+        async getAccountDetails() {
+            const accountdetails = await fetch("https://" + this.instanceurl + "/api/v1/accounts/verify_credentials", {
+                method: "GET",
+                headers: {
+                    "Authorization": "Bearer " + this.token,
+                }
+            })
+            const accountdetails_response = await accountdetails.json()
+
+            this.user.avatar = accountdetails_response.avatar;
+            this.user.displayname = accountdetails_response.display_name;
+            this.user.username = accountdetails_response.username;
+        },
+
+        async afterLogin() {
+            this.instanceurl = this.getLocalStorage("instanceurl");
+            this.token = this.getLocalStorage("token");
+
+            this.app.clientid = this.getLocalStorage("app_clientid");
+            this.app.secret = this.getLocalStorage("app_secret");
+            this.app.vapidkey = this.getLocalStorage("app_vapidkey");
+        },
+
+        async logout() {
+            console.log("Logging out...")
+            this.removeLocalStorage("token")
+            this.removeLocalStorage("instanceurl")
+            this.removeLocalStorage("loginstate")
+
+            this.removeLocalStorage("app_clientid")
+            this.removeLocalStorage("app_secret")
+            this.removeLocalStorage("app_vapidkey")
+
+            location.reload();
+        },
+
+        async sendToot() {
+            console.log("[Aester Actions] Posting toot as @" + this.user.username + "@" + this.instanceurl + "...")
+
+            const sendtoot = await fetch("https://" + this.instanceurl + "/api/v1/statuses", {
+                method: "POST",
+                headers: {
+                    "Authorization": "Bearer " + this.token,
+                    "Content-Type": "application/x-www-form-urlencoded",
+                },
+                body: `status=${this.tootarea}`,
+            })
+            const sendtoot_response = await sendtoot.json()
+
+            console.log("[Aester Actions] Posted toot. Response below.")
+            console.log(sendtoot_response)
+        }
     }
-  }
 }
 </script>
 
@@ -177,34 +180,242 @@ export default {
     </div>
 
     <div v-if="this.loginstate === 'done'">
-        <button class="loginbtn" @click="logout()">Logout</button>
+        <div class="mColumns">
+            <div class="mColumn">
+                <div class="mColumnHeader">
+                    <p>Aester <span class="betaTag">BETA</span></p>
+                </div>
+                <div class="postArea">
+                    <div class="postAreaHeader">
+                        <div>
+                            <img class="postArea-avatar" :src="user.avatar" :alt="user.displayname+'s avatar'">
+                        </div>
+                        <div class="postArea-names">
+                            <p class="postArea-displayName">{{ user.displayname }}</p>
+                            <p class="postArea-userName">@{{ user.username }}@{{ instanceurl }}</p>
+                        </div>
+                    </div>
+                    <div>
+                        <textarea class="postArea-textArea" placeholder="What's up?" v-model="tootarea"></textarea>
+                        <button class="btn" @click="sendToot()">Toot</button>
+                    </div>
+                </div>
+            </div>
+            <div class="mColumn">
+                <div class="mColumnHeader">
+                    <p>Home</p>
+                </div>
+                e
+            </div>
+            <div class="mColumn">
+                <div class="mColumnHeader">
+                    <p>Notifications</p>
+                </div>
+                e
+            </div>
+            <div class="mColumn">
+                <div class="mColumnHeader">
+                    <p class="noSelection">No Selection...</p>
+                </div>
+                e
+            </div>
+        </div>
     </div>
 </template>
 
 <style>
-    :root {
-        --bg1: #000000;
-        --bg2: #272727;
-        --bg3: #3f3f3f;
+:root {
+    --bg1: #000000;
+    --bg2: #272727;
+    --bg3: #3f3f3f;
 
-        --txt1: #ffffff;
-        --txt2: #d1d1d1;
+    --txt1: #ffffff;
+    --txt2: #d1d1d1;
 
-        --font1: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    --font1: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
 
-        --accent1: #c47ffd;
-    }
-    body { font-family: var(--font1); margin: 0px; background-color: var(--bg1); }
-    h1, h2, h3, h4, h5, h6, p { margin: 0px; }
-    button, input, textarea { font-family: var(--font1); }
-    .loginArea { display: flex; justify-content: center; }
-    .loginContainer { margin-top: 25px; width: 450px; background-color: var(--bg1); color: var(--txt1); padding: 5px; padding-left: 15px; padding-right: 15px; }
-    .loginContainerHeading { margin-top: 10px; margin-bottom: 30px; font-size: 20px; }
-    .instanceTextArea { box-sizing: border-box; width: 100%; }
-    .ipt { background-color: var(--bg2); border: 2px solid var(--bg2); color: var(--txt1); padding-left: 10px; padding-right: 10px; padding-top: 8px; padding-bottom: 8px; border-radius: 7px; }
-    .ipt:focus { outline: none; border: 2px solid var(--accent1); }
-    .iptlabel { padding-bottom: 10px; }
-    .instanceLoginButtons { margin-top: 10px; float: right }
-    .loginbtn { margin-left: 10px; background-color: var(--bg2); border: 2px solid var(--bg2); color: var(--txt1); padding-left: 10px; padding-right: 10px; padding-top: 6px; padding-bottom: 6px; border-radius: 7px; }
-    .loginbtn:hover { background-color: var(--bg3); border: 2px solid var(--bg3); }
+    --accent1: #c47ffd;
+}
+
+body {
+    font-family: var(--font1);
+    margin: 0px;
+    background-color: var(--bg1);
+}
+
+h1,
+h2,
+h3,
+h4,
+h5,
+h6,
+p {
+    margin: 0px;
+}
+
+button,
+input,
+textarea {
+    font-family: var(--font1);
+}
+
+.loginArea {
+    display: flex;
+    justify-content: center;
+}
+
+.loginContainer {
+    margin-top: 25px;
+    width: 450px;
+    background-color: var(--bg1);
+    color: var(--txt1);
+    padding: 5px;
+    padding-left: 15px;
+    padding-right: 15px;
+}
+
+.loginContainerHeading {
+    margin-top: 10px;
+    margin-bottom: 30px;
+    font-size: 20px;
+}
+
+.instanceTextArea {
+    box-sizing: border-box;
+    width: 100%;
+}
+
+.ipt {
+    background-color: var(--bg2);
+    border: 2px solid var(--bg2);
+    color: var(--txt1);
+    padding-left: 10px;
+    padding-right: 10px;
+    padding-top: 8px;
+    padding-bottom: 8px;
+    border-radius: 7px;
+}
+
+.ipt:focus {
+    outline: none;
+    border: 2px solid var(--accent1);
+}
+
+.iptlabel {
+    padding-bottom: 10px;
+}
+
+.instanceLoginButtons {
+    margin-top: 10px;
+    float: right
+}
+
+.loginbtn {
+    margin-left: 10px;
+    background-color: var(--bg2);
+    border: 2px solid var(--bg2);
+    color: var(--txt1);
+    padding-left: 10px;
+    padding-right: 10px;
+    padding-top: 6px;
+    padding-bottom: 6px;
+    border-radius: 7px;
+}
+
+.loginbtn:hover {
+    background-color: var(--bg3);
+    border: 2px solid var(--bg3);
+}
+
+.mColumns {
+    display: flex;
+}
+
+.mColumn {
+    background-color: var(--bg2);
+
+    width: 325px;
+    height: 100vh;
+
+    border-right: 1px solid var(--bg1);
+}
+
+.betaTag {
+    background-color: var(--bg3);
+    color: var(--txt1);
+
+    margin-left: 6px;
+
+    padding-left: 5px;
+    padding-right: 5px;
+    padding-top: 2px;
+    padding-bottom: 2px;
+
+    border-radius: 5px;
+
+    font-size: 12px;
+}
+
+.mColumnHeader {
+    padding: 10px;
+
+    color: var(--txt1);
+
+    border-bottom: 1px solid var(--bg1);
+}
+
+.noSelection {
+    color: var(--txt2);
+}
+
+.postAreaHeader {
+    display: flex;
+    margin-bottom: 10px;
+}
+
+.postArea {
+    margin: 10px;
+}
+
+.postArea-names {
+    display: flex;
+    flex-direction: column;
+}
+
+.postArea-displayName {
+    color: var(--txt1);
+}
+
+.postArea-userName {
+    color: var(--txt2);
+}
+
+.postArea-avatar {
+    height: 45px;
+    margin-right: 10px;
+
+    border-radius: 7px;
+}
+
+.postArea-textArea {
+    margin-top: 0px!important;
+    
+    width: 100%;
+    box-sizing: border-box;
+
+    padding: 5px;
+
+    font-size: 14px;
+
+    min-height: 32px;
+    max-height: 150px;
+    resize: vertical;
+
+    border-radius: 7px;
+    border: 2px solid var(--bg3);
+    background-color: var(--bg3);
+
+    color: var(--txt2);
+    outline: none;
+}
 </style>
