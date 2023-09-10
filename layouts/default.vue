1945f9d15f4c7108481bfdb294c8f66bfa588eb7
blueb <118416443+ihateblueb@users.noreply.github.com>
2023-09-10 17:36:51 -0400

    move everything to a layout

---
+++ layouts/default.vue
@@ -0,0 +1,342 @@
+<script setup>
+import Icon from '../components/Icon.vue';
+import Post from '../components/Post.vue';
+import PostArea from '../components/PostArea.vue';
+import Modal from '../components/Modal.vue';
+</script>
+
+<script>
+export default {
+    data: () => ({
+        code: "", // gets ignored after login
+        loginstate: 'start',
+        instanceurl: "",
+        token: "",
+        user: {
+            displayname: "none",
+            username: "none",
+            avatar: "assets/none.png",
+        },
+        app: {
+            postArea: {
+                selectedBtn: "",
+                dropdown: "",
+                poll: {
+                    options: 2
+                }
+            },
+            clientid: "",
+            secret: "",
+        },
+        toot: {
+            content: "",
+            media: [],
+            poll: {
+                options: ["", "", "", "", ""],
+                expires_in: "1 day",
+                multiple: "false",
+                hide_totals: "false",
+            },
+            sensitive: "",
+            spoiler_text: "",
+            visibility: "public",
+            language: "",
+        },
+        timeline: {
+            home: [],
+            home_new: [],
+            home_last: "",
+        }
+    }),
+    mounted() {
+        this.closeModal()
+        console.log("[Aster Startup] Login State: " + this.getLocalStorage("loginstate"))
+
+        if (this.getLocalStorage("loginstate") === 'done') {
+            this.loginstate = this.getLocalStorage("loginstate");
+            this.afterLogin()
+            this.getAccountDetails()
+            this.loadToots()
+        }
+    },
+    methods: {
+        setLocalStorage(key, value) {
+            if (process.client) {
+                localStorage.setItem(key, value)
+            }
+        },
+        getLocalStorage(key) {
+            if (process.client) {
+                return localStorage.getItem(key);
+            }
+        },
+        removeLocalStorage(key) {
+            if (process.client) {
+                return localStorage.removeItem(key);
+            }
+        },
+
+        async createApplication() {
+            const gettingapp = await fetch("https://" + this.instanceurl + "/api/v1/apps", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/x-www-form-urlencoded",
+                },
+                body: `client_name=Aster&redirect_uris=urn:ietf:wg:oauth:2.0:oob&scopes=read write push&website=https://as.blueb.me/`,
+            })
+            const gettingapp_response = await gettingapp.json()
+
+            this.app.clientid = gettingapp_response.client_id;
+            this.app.secret = gettingapp_response.client_secret;
+            this.app.vapidkey = gettingapp_response.vapid_key;
+            this.setLocalStorage("app_clientid", this.app.clientid)
+            this.setLocalStorage("app_secret", this.app.secret)
+            this.setLocalStorage("app_vapidkey", this.app.vapidkey)
+        },
+
+        async startlogin() {
+            this.removeLocalStorage("user_avatar")
+            this.removeLocalStorage("user_displayname")
+            this.removeLocalStorage("user_username")
+
+            await this.createApplication()
+
+            window.open("https://" + this.instanceurl + "/oauth/authorize?client_id=" + this.app.clientid + "&scope=read+write+push&redirect_uri=urn:ietf:wg:oauth:2.0:oob&response_type=code", '_blank');
+            this.loginstate = 'almost';
+            this.setLocalStorage("instanceurl", this.instanceurl)
+        },
+
+        async endlogin() {
+            const gettingtoken = await fetch("https://" + this.instanceurl + "/oauth/token", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/x-www-form-urlencoded",
+                },
+                body: `client_id=${this.app.clientid}&client_secret=${this.app.secret}&redirect_uri=urn:ietf:wg:oauth:2.0:oob&grant_type=authorization_code&code=${this.code}&scope=read write push`,
+            })
+            const gettingtoken_response = await gettingtoken.json()
+
+            console.log(gettingtoken_response)
+
+            this.setLocalStorage("token", gettingtoken_response.access_token)
+
+            this.token = gettingtoken_response.access_token;
+
+            this.getAccountDetails()
+            this.afterLogin()
+
+            this.setLocalStorage("loginstate", "done")
+            this.loginstate = 'done';
+        },
+
+        async getAccountDetails() {
+            const accountdetails = await fetch("https://" + this.instanceurl + "/api/v1/accounts/verify_credentials", {
+                method: "GET",
+                headers: {
+                    "Authorization": "Bearer " + this.token,
+                }
+            })
+            const accountdetails_response = await accountdetails.json()
+
+            this.user.avatar = accountdetails_response.avatar;
+            this.user.displayname = accountdetails_response.display_name;
+            this.user.username = accountdetails_response.username;
+
+            this.setLocalStorage("user_avatar", this.user.avatar)
+            this.setLocalStorage("user_displayname", this.user.displayname)
+            this.setLocalStorage("user_username", this.user.username)
+        },
+
+        async afterLogin() {
+            this.instanceurl = this.getLocalStorage("instanceurl");
+            this.token = this.getLocalStorage("token");
+
+            this.app.clientid = this.getLocalStorage("app_clientid");
+            this.app.secret = this.getLocalStorage("app_secret");
+            this.app.vapidkey = this.getLocalStorage("app_vapidkey");
+        },
+
+        async logout() {
+            console.log("Logging out...")
+
+            this.removeLocalStorage("user_avatar")
+            this.removeLocalStorage("user_displayname")
+            this.removeLocalStorage("user_username")
+
+            this.removeLocalStorage("token")
+            this.removeLocalStorage("instanceurl")
+            this.removeLocalStorage("loginstate")
+
+            this.removeLocalStorage("app_clientid")
+            this.removeLocalStorage("app_secret")
+            this.removeLocalStorage("app_vapidkey")
+
+            this.removeLocalStorage("ui_modal")
+
+            location.reload();
+        },
+
+        async openModal(modal) {
+            this.setLocalStorage("ui_modal", modal)
+        },
+        async closeModal() {
+            this.removeLocalStorage("ui_modal")
+        },
+
+        async loadToots() {
+            let initialtoots = await fetch("https://" + this.instanceurl + "/api/v1/timelines/home?limit=40", {
+                method: "GET",
+                headers: {
+                    "Authorization": "Bearer " + this.token,
+                }
+            })
+            let initialtoots_response = await initialtoots.json()
+
+            initialtoots_response.forEach((element) =>
+                this.timeline.home.push(element) &&
+                console.log(element)
+            )
+
+            this.timeline.home_last = initialtoots_response.at(-1).id;
+
+            this.startStream()
+        },
+        async loadMoreToots(id) {
+            let moretoots = await fetch("https://" + this.instanceurl + "/api/v1/timelines/home?max_id=" + id + "?limit=40", {
+                method: "GET",
+                headers: {
+                    "Authorization": "Bearer " + this.token,
+                }
+            })
+            let moretoots_response = await moretoots.json()
+
+            moretoots_response.forEach((element) =>
+                this.timeline.home.push(element)
+            )
+
+            this.timeline.home_last = moretoots_response.at(-1).id;
+        },
+        async resetFeed() {
+            this.timeline.home = [];
+            this.loadToots()
+        },
+
+        async startStream() {
+            let userSocket = new WebSocket("wss://" + this.instanceurl + "/api/v1/streaming?access_token=" + this.token + "&stream=user");
+
+            userSocket.onmessage = (event) => {
+                let msg = JSON.parse(event.data)
+                this.timeline.home_new.push(JSON.parse(msg.payload))
+            }
+        },
+
+        async onScroll(e) {
+            const { scrollTop, offsetHeight, scrollHeight } = e.target
+            if ((scrollTop + offsetHeight) >= scrollHeight) {
+                this.loadMoreToots(this.timeline.home_last)
+            }
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="modalContainer" v-if="getLocalStorage('ui_modal') === 'settings'">
+        <Modal :name="getLocalStorage('ui_modal')" />
+    </div>
+
+    <div v-if="this.loginstate !== 'done'">
+        <div class="loginArea" v-if="this.loginstate === 'start'">
+            <div class="loginContainer">
+                <div class="loginContainerHeader">
+                    <h1 class="loginContainerHeading">Welcome to Aster</h1>
+                </div>
+                <div>
+                    <p class="iptlabel">Please type your instances URL</p>
+                    <input type="text" placeholder="eg. wetdry.world" class="ipt instanceTextArea" v-model="instanceurl">
+                    <div class="instanceLoginButtons">
+                        <button class="loginbtn" @click="startlogin()">Next</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="loginArea" v-if="this.loginstate === 'almost'">
+            <div class="loginContainer">
+                <div class="loginContainerHeader">
+                    <h1 class="loginContainerHeading">Welcome to Aster</h1>
+                </div>
+                <div>
+                    <p class="iptlabel">Please type in your Authorization Code</p>
+                    <input type="text" placeholder="Authorization Code" class="ipt instanceTextArea" v-model="code">
+                    <div class="instanceLoginButtons">
+                        <button class="loginbtn" @click="logout()">Cancel</button>
+                        <button class="loginbtn" @click="endlogin()">Finish</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div v-if="this.loginstate === 'done'">
+        <div class="mColumns">
+            <div class="mColumn">
+                <div class="mColumnHeader">
+                    <div class="mCH-text">
+                        <p>Aster <span class="betaTag">BETA</span></p>
+                    </div>
+                    <div class="mCH-buttons">
+                        <button @click="openModal('settings')" class="btn btn-header">
+                            <Icon name="settings" size="16px" />
+                        </button>
+                        <button @click="logout()" class="btn btn-header-logout">
+                            <Icon name="log-out" size="16px" />
+                        </button>
+                    </div>
+                </div>
+                <div class="mColumnContent">
+                    <PostArea />
+
+                    <div class="mCHbottom">
+                        <img class="mCHbottomImage"
+                            src="https://media.wetdry.world/site_uploads/files/000/000/002/original/skeeter.png"
+                            v-if="this.instanceurl === 'wetdry.world'">
+                    </div>
+                </div>
+            </div>
+            <div class="mColumn">
+                <div class="mColumnHeader">
+                    <div class="mCH-text">
+                        <p>Home</p>
+                    </div>
+                    <div class="mCH-buttons">
+                        <button @click="resetFeed()" class="btn btn-header">
+                            <Icon name="refresh-ccw" size="16px" />
+                        </button>
+                    </div>
+                </div>
+                <div class="mColumnContent" @scroll="onScroll">
+                    <div class="timelineNewPosts">
+                        <div v-for="toot in this.timeline.home_new">
+                            <Post :data="toot" :instanceurl="this.instanceurl" :token="this.token" />
+                        </div>
+                    </div>
+                    <div v-for="toot in this.timeline.home">
+                        <Post :data="toot" :instanceurl="this.instanceurl" :token="this.token" />
+                    </div>
+                </div>
+            </div>
+            <div class="mColumn">
+                <div class="mColumnHeader">
+                    <p>Notifications</p>
+                </div>
+                e
+            </div>
+            <div class="mColumn">
+                <div class="mColumnHeader">
+                    <p class="noSelection">No Selection...</p>
+                </div>
+                <slot></slot>
+            </div>
+        </div>
+    </div>
+</template>
