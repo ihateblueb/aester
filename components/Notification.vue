f34ca11fc278668727e4655f0930504861a6e034
blueb <118416443+ihateblueb@users.noreply.github.com>
2023-09-21 20:32:13 -0400

    follow requests 🔥🔥🔥

--- components/Notification.vue
+++ components/Notification.vue
@@ -6,26 +6,70 @@ import Icon from '../components/Icon.vue';
 export default {
     data: () => ({
         content: [],
+        token: "",
+        fr_notaccepted: true,
         ready: false,
         showCwContent: false
     }),
     props: {
         instanceurl: String,
-        token: String,
         data: Object,
     },
     mounted() {
         this.content = this.data
+        this.token = this.getLocalStorage("token")
         this.ready = true
     },
     methods: {
+        setLocalStorage(key, value) {
+            if (process.client) {
+                localStorage.setItem(key, value)
+            }
+        },
+        getLocalStorage(key) {
+            if (process.client) {
+                return localStorage.getItem(key);
+            }
+        },
+        removeLocalStorage(key) {
+            if (process.client) {
+                return localStorage.removeItem(key);
+            }
+        },
 
+        async followRequest(response, id) {
+            if (response === 'accept') {
+                const acceptfollowrequest = await fetch("https://" + this.instanceurl + "/api/v1/follow_requests/" + id + "/authorize", {
+                    method: "POST",
+                    headers: {
+                        "Authorization": "Bearer " + this.token,
+                    }
+                })
+                const acceptfollowrequest_response = await acceptfollowrequest.json()
+
+                this.fr_notaccepted = false
+
+                console.log(acceptfollowrequest_response)
+            } else if (response === 'deny') {
+                const denyfollowrequest = await fetch("https://" + this.instanceurl + "/api/v1/follow_requests/" + id + "/reject", {
+                    method: "POST",
+                    headers: {
+                        "Authorization": "Bearer " + this.token,
+                    }
+                })
+                const denyfollowrequest_response = await denyfollowrequest.json()
+
+                this.fr_notaccepted = true
+
+                console.log(denyfollowrequest_response)
+            }
+        }
     }
 }
 </script>
 
 <template>
-    <div class="post" v-if="this.ready">
+    <div class="post" v-if="this.ready && fr_notaccepted">
         <div v-if="this.content.type === 'favourite'">
             <div class="notificationTop">
                 <Icon type="star" size="18px" color="var(--favorite)" fill=true />
@@ -86,8 +130,17 @@ export default {
                 <span v-html="content.status.content"></span>
             </div>
         </div>
+        <div v-if="this.content.type === 'follow_request'">
+            <div class="notificationTop">
+                <Icon type="user" size="18px" color="var(--accent1)" />
+                <span><NuxtLink class="notificationMention" :to="'/@'+content.account.acct">{{ content.account.display_name }}</NuxtLink> requested to follow you</span>
+            </div>
+            <div class="notificationStatus">
+                <a class="acceptFollowRequest" @click="followRequest('accept', content.account.id)">Accept</a><a class="denyFollowRequest" @click="followRequest('deny', content.account.id)">Deny</a>
+            </div>
+        </div>
         <div
-            v-if="this.content.type !== 'reblog' && this.content.type !== 'favourite' && this.content.type !== 'follow' && this.content.type !== 'mention' && this.content.type !== 'status' && this.content.type !== 'poll' && this.content.type !== 'reaction'">
+            v-if="this.content.type !== 'reblog' && this.content.type !== 'favourite' && this.content.type !== 'follow' && this.content.type !== 'mention' && this.content.type !== 'status' && this.content.type !== 'poll' && this.content.type !== 'reaction' && this.content.type !== 'follow_request'">
             <div class="notificationTop">
                 <Icon type="alert-circle" size="18px" color="var(--bg-danger)" />
                 <span>{{ content }}</span>
@@ -97,6 +150,17 @@ export default {
 </template>
 
 <style>
+.denyFollowRequest {
+    color: var(--bg-danger);
+    cursor: pointer;
+    margin-left: 5px;
+}
+.acceptFollowRequest {
+    color: var(--bg-success);
+    cursor: pointer;
+    margin-right: 5px;
+}
+
 .notificationTop {
     font-size: 16px;
 }
