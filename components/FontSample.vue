fbca6cbde53524a5c93e7cf3476b81114de1a03f
ihateblueb <harperssademailaddress@gmail.com>
2023-10-24 19:52:28 -0400

    update fontsample.vue

--- components/FontSample.vue
+++ components/FontSample.vue
@@ -8,7 +8,7 @@ export default {
 
 <template>
     <div class="fontPreview">
-        <span :class="'ft_'+font+' fontPreviewText'">The quick brown fox jumps over the sleepy dog.</span>
+        <span :class="'ft_'+font+' fontPreviewText'">The quick brown fox jumps over the lazy dog.</span>
     </div>
 </template>
 
@@ -19,5 +19,6 @@ export default {
 }
 .fontPreviewText {
     font-family: var(--font1);
+    color: var(--txt2);
 }
 </style>
